c55b42b420fbc7e4d0bf046230cc1d60ebb1af45
PhilippeL93 <PLoubiere93@gmail.com>
2019-01-31 19:08:35 +0100

    Initial Commit

---
+++ FirstPlayWithSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FirstPlayWithSwift
+//
+//  Created by Philippe on 31/01/2019.
+//  Copyright © 2019 Philippe. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
